e118bb829637a636bb5f0a98ebe77fc8ca1f6eb7
Sean Drucker <srdrucker@gmail.com>
2021-06-06 23:04:40 -0400

    cleanup

--- git.sh
+++ git.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+# shellcheck disable=SC1091,SC2034,SC2269
 . shmod
 import dockcmd/sh@v0.0.4 docker.sh
 
-t9t=
+t9t="$t9t"
 
 docker_run alpine/git "$@"
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# shellcheck disable=SC1091,SC2034
+# shellcheck disable=SC1091,SC2034,SC2269
 . shmod
 import github.com/dockcmd/sh@v0.0.5 docker.sh
 
